9b6cd6cbe0da886c61be52cb106761b0a0ad8a6c
ASHLOUISE <paluzziashley@gmail.com>
2024-03-11 19:27:55 -0700

    api files added

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+
+
+
+//router.use((req, res) => {
+    //res.send("Wrong Route!");
+//}); 
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-
+// routes
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// routes
 
 
 db.once('open', () => {
