bcc9a95fc8eee133f2bb70911f9f7b290a2d33b8
ASHLOUISE <paluzziashley@gmail.com>
2024-03-12 09:02:33 -0700

    router GET all id added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,17 +4,18 @@ const { User } = require('../../models');
 
 
 //api/users
+//get all users
 router.get("/", async (req, res) => {
     try {
         const dbUserData = await User.find().select("-__v");
-        res.status(200).json(dbUserData);
+        return res.status(200).json(dbUserData);
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 });
 
-
+//creates a new user
 router.post("/", async (req, res) => {
     try {
         const dbUserData = await User.create(req.body);
@@ -26,6 +27,23 @@ router.post("/", async (req, res) => {
 });
 
 
+//api/users/:id
+//get a single user by id
+router.get("/:id", async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({ _id: req.params.id })
+        .select("-__v")
+        .populate("friends")
+        .populate("thoughts");
+        if (!dbUserData) {
+            return res.status(404).json({ message: "No user found with this id!" });
+        }
+        res.status(200).json(dbUserData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
