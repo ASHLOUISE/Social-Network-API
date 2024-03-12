88d80a9db461ea49b9dd8e5cf013ec9d00cdc02b
ASHLOUISE <paluzziashley@gmail.com>
2024-03-12 09:54:45 -0700

    friend id routes added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const { User } = require('../../models');
-
-
+const { User, Thought } = require('../../models');
+const { findOneAndDelete } = require('../../models/user');
 
 //api/users
 //get all users
@@ -44,6 +43,97 @@ router.get("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+
+router.put("/:id", async (req, res) => {
+    try {
+        const dbUserData = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        );
+        if (!dbUserData) {
+            return res.status(404).json({ message: "No user found with this id!" });
+        }
+        res.status(200).json(dbUserData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+}
+);
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const dbUserData = await User.findOneAndDelete({ _id: req.params.id });
+        if (!dbUserData) {
+            return res.status(404).json({ message: "No user found with this id!" });
+        }
+        await Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+        res.status(200).json({ message: "User and associated thoughts deleted!" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+}
+);
+
+
+//api/users/:id/friends/:friendId
+
+router.post("/:id/friends/:friendId", async (req, res) => {
+    try {
+        const dbUserData = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        );
+        if (!dbUserData) {
+            return res.status(404).json({ message: "No user found with this id!" });
+        }
+        const dbFriendData = await User.findOneAndUpdate(
+            { _id: req.params.friendId },
+            { $addToSet: { friends: req.params.id } },
+            { runValidators: true, new: true }
+        );
+        if (!dbFriendData) {
+            return res.status(404).json({ message: "No friend found with this id!" });
+        }
+        res.status(200).json({ message: "Friend added!" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+}
+);
+
+router.delete("/:id/friends/:friendId", async (req, res) => {
+    try {
+        const dbUserData = await User.findOneAndDelete;(
+            { _id: req.params.id },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        );
+        if (!dbUserData) {
+            return res.status(404).json({ message: "No user found with this id!" });
+        }
+        const dbFriendData = await User.findOneAndDelete(
+            { _id: req.params.friendId },
+            { $pull: { friends: req.params.id } },
+            { runValidators: true, new: true }
+        );
+        if (!dbFriendData) {
+            return res.status(404).json({ message: "No friend found with this id!" });
+        }
+        res.status(200).json({ message: "Friend deleted!" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+}
+);
+
 
 
 module.exports = router;
