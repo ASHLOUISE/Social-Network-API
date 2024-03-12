b246158c991669485c4d9402085a75448be89801
ASHLOUISE <paluzziashley@gmail.com>
2024-03-11 19:21:14 -0700

    connection added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const db = require('./config/connection');
 
 
 const PORT = process.env.PORT || 3001;
@@ -10,10 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// db.once('open', () => {
-app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
 });
-// });
 
 
